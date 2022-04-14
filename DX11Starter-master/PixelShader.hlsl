0e5a43e94f2bcd8da42272b8fb330881f0796e49
RegularStiv <sar7743@rit.edu>
2022-04-14 16:43:27 -0400

    push for home

--- DX11Starter-master/PixelShader.hlsl
+++ DX11Starter-master/PixelShader.hlsl
@@ -3,6 +3,8 @@
 Texture2D SurfaceTexture	:		register(t0); // "t" registers for textures
 Texture2D SpecularTexture	:		register(t1);
 Texture2D NormalTexture		:		register(t2);// "t" registers for textures
+Texture2D MetalTexture		:		register(t3);
+Texture2D RoughnessTexture	:		register(t4);
 SamplerState BasicSampler	:		register(s0); // "s" registers for samplers
 
 cbuffer ExternalData : register(b0)
@@ -43,35 +45,56 @@ float4 main(VertexToPixel_NormalMap input) : SV_TARGET
 	input.normal = normalize(mul(unpackedNormal, TBN)); // Note multiplication order!
 	//return float4(input.normal, 1);
 	// Adjust the variables below as necessary to work with your own code
-	float3 surfaceColor = SurfaceTexture.Sample(BasicSampler, input.uv).rgb;
+	float3 surfaceColor = pow(SurfaceTexture.Sample(BasicSampler, input.uv).rgb, 2.2f);
 	surfaceColor *= colorTint;
 	float specularScale = SpecularTexture.Sample(BasicSampler, input.uv).r;
-	//return float4(specularScale, 0, 0, 1);
+
+	float roughness = RoughnessTexture.Sample(BasicSampler, input.uv).r;
+	float metalness = MetalTexture.Sample(BasicSampler, input.uv).r;
+	// Specular color determination -----------------
+// Assume albedo texture is actually holding specular color where metalness == 1
+//
+// Note the use of lerp here - metal is generally 0 or 1, but might be in between
+// because of linear texture sampling, so we lerp the specular color to match
+	float3 specularColor = lerp(F0_NON_METAL.rrr, surfaceColor.rgb, metalness);
+
+
+
+	//directional lights
 	float3 negDirectionNormal = normalize(-lights[0].Direction);
-	float3 diffuse =  (Diffuse(input.normal, negDirectionNormal));
-	float light = Phong(input.normal, input.worldPosition, MAX_SPECULAR_EXPONENT, roughness, cameraPosition, -negDirectionNormal) * specularScale;
-	float3 finalColor = float3(((diffuse  * lights[0].Color * surfaceColor) + (ambient * surfaceColor))) + light;
+
+	float diffuse =  (DiffusePBR(input.normal, negDirectionNormal));
+	float3 spec = MicrofacetBRDF(input.normal, negDirectionNormal, normalize(cameraPosition - input.worldPosition), roughness, specularColor);
+	float3 balancedDiff = DiffuseEnergyConserve(diffuse, spec, metalness);
+
+	float3 finalColor = (balancedDiff * surfaceColor + spec) * lights[0].Intensity * lights[0].Color;
 
 	negDirectionNormal = normalize(-lights[1].Direction);
-	diffuse = (Diffuse(input.normal, negDirectionNormal));
-	light = Phong(input.normal, input.worldPosition, MAX_SPECULAR_EXPONENT, roughness, cameraPosition, -negDirectionNormal) * specularScale;
-	float3 finalColor2 = float3(((diffuse * lights[1].Color * surfaceColor) + (ambient * surfaceColor))) + light;
+	diffuse = (DiffusePBR(input.normal, negDirectionNormal));
+	spec = MicrofacetBRDF(input.normal, negDirectionNormal, normalize(cameraPosition - input.worldPosition), roughness, specularColor);
+	float3 finalColor2 = (balancedDiff * surfaceColor + spec) * lights[1].Intensity * lights[1].Color;
 
 	negDirectionNormal = normalize(-lights[2].Direction);
-	diffuse = (Diffuse(input.normal, negDirectionNormal));
-	light = Phong(input.normal, input.worldPosition, MAX_SPECULAR_EXPONENT, roughness, cameraPosition, -negDirectionNormal) * specularScale;
-	float3 finalColor3 = float3(((diffuse * lights[2].Color * surfaceColor) + (ambient * surfaceColor))) + light;
+	diffuse = (DiffusePBR(input.normal, negDirectionNormal));
+	spec = MicrofacetBRDF(input.normal, negDirectionNormal, normalize(cameraPosition - input.worldPosition), roughness, specularColor);
+	float3 finalColor3 = (balancedDiff * surfaceColor + spec) * lights[2].Intensity * lights[2].Color;
 
+
+	//point lights
 	float3 direction = normalize(lights[3].Position - input.worldPosition);
-	diffuse = (Diffuse(input.normal, direction)) * Attenuate(lights[3], input.worldPosition);
-	light = Phong(input.normal, input.worldPosition, MAX_SPECULAR_EXPONENT, roughness, cameraPosition, -direction) * Attenuate(lights[3], input.worldPosition) * specularScale;
-	float3 finalColor4 = float3(((diffuse * lights[3].Color * surfaceColor) + (ambient * surfaceColor))) + light;
+	diffuse = (DiffusePBR(input.normal, direction)) * Attenuate(lights[3], input.worldPosition);
+	spec = MicrofacetBRDF(input.normal, direction, normalize(cameraPosition - input.worldPosition), roughness, specularColor);
+	float3 finalColor4 = (balancedDiff * surfaceColor + spec) * lights[3].Intensity * lights[3].Color;
 
 	direction = normalize(lights[4].Position - input.worldPosition);
-	diffuse = (Diffuse(input.normal, direction)) * Attenuate(lights[4], input.worldPosition);
-	light = Phong(input.normal, input.worldPosition, MAX_SPECULAR_EXPONENT, roughness, cameraPosition, -direction) * Attenuate(lights[4], input.worldPosition) * specularScale;
-	float3 finalColor5 = float3(((diffuse * lights[4].Color * surfaceColor) + (ambient * surfaceColor))) + light;
+	diffuse = (DiffusePBR(input.normal, direction)) * Attenuate(lights[4], input.worldPosition);
+	spec = MicrofacetBRDF(input.normal, direction, normalize(cameraPosition - input.worldPosition), roughness, specularColor);
+	float3 finalColor5 = (balancedDiff * surfaceColor + spec) * lights[4].Intensity * lights[4].Color;
 
-	return float4(finalColor + finalColor2 + finalColor3 + finalColor4 + finalColor5, 1);
+	//add all lights
+	float3 totalColor = finalColor + finalColor2 + finalColor3 + finalColor4 + finalColor5;
+
+	//return all lights with gamma correction
+	return float4(pow(totalColor, 1.0f / 2.2f), 1);
 
 }
